fd137a5c9b985fcb61c3eb795d6fdcc4d32711ee
puppy2111 <huynhdaihuybank3@gmail.com>
2023-03-10 21:10:08 +0700

    update format schema

--- app/models/user.model.js
+++ app/models/user.model.js
@@ -6,27 +6,38 @@ const User = mongoose.model(
         username: {
             type: String,
             required: true,
-            unique: true
+            unique: true,
+            minLength: 8
         },
         email: {
             type: String,
             required: true,
-            unique: true
+            unique: [true, 'User"s email required'],
+            match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Format of email is invalid !']
+
         },
         name: {
             type: String,
-            required: true,
+            required: [true, 'User"s name required'],
+            maxLength: 30
+
         },
         phone: {
             type: String,
             required: true,
-            unique: true,
+            unique: [true, 'User"s phone number required'],
+            maxLength: 10,
+            match: [/0[35789]\d{8}$/, 'Format of phone is invalid !']
         },
         password: {
             type: String,
-            required: true,
+            required: [true, "User's password required"],
+            minLength: 8
         },
-        address: String,
+        address: {
+            type: String,
+            maxLength: 200
+        },
         roles: [{
             type: mongoose.Schema.Types.ObjectId,
             ref: "Role"
